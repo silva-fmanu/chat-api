54391f5f8204c0e12710e39d8113412789db954e
silva-fmanu <silvafarias.manu07@gmail.com>
2024-07-03 17:05:32 -0300

    Estrutura da api

---
+++ src/api.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+app.use(express.urlencoded({extend : true}));
+app.use(express.json());
+
+const router = express.Router();
+app.use('/', router.get('/', (req, res)=>{
+    res.status(200).send("<h1>API - CHAT</h1>")
+}))
+
+module.exports = app;
